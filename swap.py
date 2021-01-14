c560d53b188b057d9089f6f81623ff662f635171
cpe9210 <cpe9210@users.noreply.github.com>
2021-01-14 20:07:56 +0300

    swap

---
+++ swap.py
@@ -0,0 +1,24 @@
+#way1
+a = 20
+b = 60
+a,b =b,a
+print("The value of a:",a)
+print('The value of b:',b)
+
+#way2
+x = 30
+y =90
+swap = x
+x = y
+y = swap
+print("The value of x:",x)
+print('The value of y:',y)
+
+#way3
+c = 80
+g = 10
+c = c^g
+g = c^g
+c = c^g
+print('The value of c:', c)
+print('The value of g:',g)
